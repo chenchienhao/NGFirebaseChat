4f550ca0fcf37e5b60c1f64bc40d2b27ed31e59d
Hao <animax801213@gmail.com>
2017-05-16 15:08:39 -0400

    css changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,10 +39,21 @@ export class AppComponent {
         this.name = auth;
       }
     });
-
-    
   }
-
+  move() {
+    var elem = document.getElementById("myBar");   
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+      } else {
+        width++; 
+        elem.style.width = width + '%'; 
+        elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
   login(){
     this.af.auth.login({
       provider: AuthProviders.Facebook,
